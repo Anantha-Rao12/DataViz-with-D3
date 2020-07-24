9ef596a228a75de71b4ac051a0c01637aae2f9d0
Anantha S Rao <aanantha.s.rao@gmail.com>
2020-07-24 23:55:44 +0530

    Changed loadPreprocess to index.js
    This new file contains source code for a happy face :) 
    Changes are not file. Need to branch out and work soon

--- loadAndProcessData.js
+++ loadAndProcessData.js
@@ -1,23 +1,55 @@
-import { feature } from 'topojson';
-import { csv, json} from 'd3';
-export const loadAndProcessData  = () =>
-  Promise
-    .all([
-      csv('deaths_per_capita.csv'),
-      json('https://unpkg.com/world-atlas@1.1.4/world/50m.json')
-    ])
-    .then(([unData, topoJSONdata])=> {
-      console.log(unData)
+
+const svg = d3.select('svg');
 
 
+const height = +svg.attr('height'); 
+const width = +svg.attr('width');
 
-      /*
-      const rowById = tsvData.reduce((accumulator,))
-    }
-    
-    
-    
-    
-    */
-    
-  });
+console.log(width)
+
+const g = svg.append('g')
+  .attr('transform','translate(480,250)');
+
+const circle = g.append('circle')
+  .attr('r',height/2)
+  .attr('fill','pink')
+  .attr('stroke','black');
+
+const eyeradius = 30;
+const eyespacing = 100;
+const eyeYOffset = -70;
+const eyebrowwidth = 70;
+const eyebrowheight = 15;
+const eyebrowYOffset = -70;
+
+
+const eyesG = g.append('g')
+.attr('transform','translate(0,-70)');
+
+const leftEye = eyesG.append('circle')
+  .attr('r',eyeradius)
+  .attr('cx', - eyespacing)
+  
+const RightEye = eyesG.append('circle')
+  .attr('r',eyeradius)
+  .attr('cx', + eyespacing)
+
+const leftEyebrow = eyesG.append('rect')
+  .attr('x', -eyespacing - eyebrowwidth/2)
+  .attr('y', eyebrowYOffset)
+  .attr('width', eyebrowwidth)
+  .attr('height',eyebrowheight)
+
+const rightEyebrow = eyesG.append('rect')
+  .attr('x', +eyespacing - eyebrowwidth/2)
+  .attr('y',eyebrowYOffset)
+  .attr('width', eyebrowwidth)
+  .attr('height',eyebrowheight)
+
+const mouth = g.append('path')
+  .attr('d',d3.arc()({
+    innerRadius : 150,
+    outerRadius : 170,
+    startAngle:Math.PI/2,
+    endAngle: Math.PI*3/2
+  }));
